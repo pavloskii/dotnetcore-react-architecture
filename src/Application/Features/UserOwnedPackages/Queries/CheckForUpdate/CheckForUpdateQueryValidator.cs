c35b96bb61479ed8a6ab1a777eed4eb0f9f95804
pavloskii <pavlo.igor@gmail.com>
2020-11-09 13:33:01 +0100

    CheckUpdate Validator

---
+++ src/Application/Features/UserOwnedPackages/Queries/CheckForUpdate/CheckForUpdateQueryValidator.cs
@@ -0,0 +1,21 @@
+﻿using FDS.Application.Features.UserOwnedPackages.Queries.CheckForUpdate;
+using FDS.Application.Interfaces;
+using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FDS.Application.TodoLists.Commands.CreateTodoList
+{
+    public class CheckForUpdateQueryValidator : AbstractValidator<CheckForUpdateQuery>
+    {
+        public CheckForUpdateQueryValidator()
+        {
+            RuleFor(v => v.PackageId)
+                .NotEmpty().WithMessage("PackageId is required.");
+
+            RuleFor(v => v.FromPackageVersionId)
+                .NotEmpty().WithMessage("FromPackageVersionId is required");
+        }
+    }
+}
